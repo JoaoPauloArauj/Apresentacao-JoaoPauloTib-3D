2078df9bc541e544e4941e39af2e99165e3c45a1
João Paulo de Araujo Tiburcio <124619394+JoaoPauloArauj@users.noreply.github.com>
2024-08-29 01:27:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const primeiroNome ="João Paulo"
+const sobrenome = "Tiburcio"
+const idade = 17;
+const altura = 1.86;
+const massa = 79;
+const cidadeNatal = "Curitiba";
+
+console. log("Meu primeiro nome é:");
+console. log(primeiroNome);
+
+console. log("O nome da minha família é:")
+console. log(sobrenome);
+
+console. log("A minha idade é:");
+console. log(idade);
+
+console. log("A minha altura é:");
+console. log(altura);
+
+console. log("A minha massa é:");
+console. log(massa);
+
+console. log("Minha cidade natal é:");
+console. log(cidadeNatal);
